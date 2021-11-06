1c0dbd0c9ad72b2539ba765f825f8922faaefc9c
yiannisha <hadjiyiannis.3@gmail.com>
2021-11-06 19:26:15 +0200

    Added tests for shellinter.get_stdout

--- tests/test_shellinter.py
+++ tests/test_shellinter.py
@@ -1,7 +1,7 @@
 
 """ A module to test the shellinter module """
 
-from shellinter import readFromBufferedReader
+import shellinter
 
 import subprocess
 from subprocess import PIPE
@@ -9,26 +9,56 @@ import unittest
 
 class Test(unittest.TestCase):
 
-    def setUp(self):
-        """ Set up a process for testing """
+    def setUp(self) -> None:
+        """ Set up a processes for testing """
 
-        self.process = subprocess.Popen('/bin/bash',
-                                        shell=True,
-                                        stdin=PIPE,
-                                        stdout=PIPE,
-                                        stderr=PIPE,
-                                        bufsize=10)
+        self.psql_process = subprocess.Popen('/bin/bash',
+                                             shell=True,
+                                             stdin=PIPE,
+                                             stdout=PIPE,
+                                             stderr=PIPE,
+                                             bufsize=10)
 
-    def _process_write(self, num: int) -> None:
-        """ Helper function for filling stdout """
+        self.mysql_process = subprocess.Popen('/bin/bash',
+                                              shell=True,
+                                              stdin=PIPE,
+                                              stdout=PIPE,
+                                              stderr=PIPE,
+                                              bufsize=10)
+        self.process_tests = [
+            {
+                'process' : self.psql_process,
+                'test_quaries' : ['select datname from pg_database;\n'],
+                'login' : 'psql -h localhost',
+                'type' : 'psql',
+            },
+            {
+                'process' : self.mysql_process,
+                'test_quaries' : ['select DATABASE();\n'],
+                'login' : 'mysql --login-path=local\n',
+                'type' : 'mysql',
+            },
+        ]
 
-        for i in range(int):
-            self.process.sdout.write('echo "test"\n')
+        for process in self.process_tests:
+                process['process'].stdin.write(process['login'])
 
-    def test_readFromBufferedReader(self):
-        """ Tests shellinter.readFromBufferedReader """
+    def test_get_stdout(self):
+        """ Tests shellinter.get_stdout """
 
         # tests raised exceptions
-        self.assertRaises(readFromBufferedReader, '')
 
-        # test output type
+        # local databases
+        known_values = [
+             # default mysql local databases
+            '+------------+\n| DATABASE() |\n+------------+\n| NULL       |\n+------------+',
+             # default postgresql local databases
+            '  datname  \n-----------\n postgres\n template1\n template0\n(3 rows)\n',
+        ]
+
+        # test output
+        for test_process, known in zip(self.process_tests, known_values):
+            for process, quaries, _, type in test_process:
+                state_output = shellinter.get_stdout(process, quaries, type, capture_output=True)
+                self.assertEqual(state_output, known)
+        
