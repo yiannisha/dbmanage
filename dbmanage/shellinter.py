e78992281fc888374fc3691b89e784cac05480fe
yiannisha <hadjiyiannis.3@gmail.com>
2021-11-10 16:32:53 +0200

    Added query_utils.py and refactored shellinter.py

--- dbmanage/shellinter.py
+++ dbmanage/shellinter.py
@@ -4,7 +4,9 @@
 import os
 import subprocess
 
-# main module functions
+from query_utils import parse_connection_request
+
+# API
 
 def connect(dbtype: str, **kwargs) -> subprocess.Popen:
     """ Creates a connection to the database server """
@@ -13,7 +15,7 @@ def connect(dbtype: str, **kwargs) -> subprocess.Popen:
     process = subprocess.Popen('/bin/bash', shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=10)
 
     # connect process to database server
-    cmd = _parse_connection_request(dbtype, **kwargs)
+    cmd = parse_connection_request(dbtype, **kwargs)
 
     # debug
     #print(cmd)
@@ -23,45 +25,3 @@ def connect(dbtype: str, **kwargs) -> subprocess.Popen:
     return process
 
 # helper functions
-
-def _parse_connection_request(dbtype: str, **kwargs) -> str:
-    """ Returns a string command to be run by a subprocess """
-
-    connection = ''
-
-    if dbtype == 'mysql':
-        connection = _parse_mysql_connection_request(**kwargs)
-    elif dbtype == 'postgres':
-        connection = _parse_psql_connection_request(**kwargs)
-
-    return connection
-
-def _parse_mysql_connection_request(**kwargs) -> str:
-
-    base_cmd = ''
-    try:
-        pass_str = ''
-        if kwargs['pass']:
-            pass_str = f'-p{kwargs["pass"]}'
-
-        base_cmd = f'mysql -h {kwargs["host"]} -u {kwargs["user"]} {pass_str} {kwargs["dbname"]}\n'
-    except KeyError as e:
-        # TODO: properly handle missing args
-        raise ValueError('')
-
-    return base_cmd
-
-def _parse_psql_connection_request(**kwargs) -> str:
-
-    base_cmd = ''
-    try:
-        pass_str = ''
-        if kwargs['pass']:
-            pass_str = f'--password{kwargs["pass"]}'
-
-        base_cmd = f'psql -h {kwargs["host"]} -U {kwargs["user"]} -p {kwargs["port"]} {pass_str} {kwargs["dbname"]}\n'
-    except KeyError as e:
-        # TODO: properly handle missing args
-        raise ValueError('')
-
-    return base_cmd
