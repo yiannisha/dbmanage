66a3e18aac951a2f41eafa5d36bcc9d5296c8d0b
yiannisha <hadjiyiannis.3@gmail.com>
2021-12-01 21:31:06 +0200

    Refactor output reading to query_utils.py from connection.py

--- dbmanage/connection.py
+++ dbmanage/connection.py
@@ -2,6 +2,7 @@
 """ A module for the Connection Classes """
 
 from dbmanage import shellinter
+from dbmanage import query_utils
 from dbmanage.database import Database
 
 import os
@@ -44,80 +45,7 @@ class Connection():
         and the values of each row as elements in a list as dictionary values
         """
 
-        return self._untabulate(self._readrows(*args, **kwargs))
-
-    def _readrows(self, filepath: str, extra: bool = True) -> Dict:
-        """
-        Returns a dictionary with "column", "row", "extra" as keys
-        and the lines of the file that contain each one as dictionary values.
-
-        This function is to be called in self._readStdout and this code was
-        refactored so that _readStdout could be overwritten without the need of
-        rewriting the whole function.
-        """
-
-        if not os.path.exists(filepath):
-            raise FileNotFoundError(f'File with query output not found.\npath: {filepath}')
-
-        with open(filepath, 'r', encoding='utf-8') as f:
-
-            column_str = ''
-            row_strs = []
-            extra_strs = []
-
-            # read column line
-            column_str = f.readline()
-            while not self._isrow(column_str):
-                column_str = f.readline()
-                if not column_str:
-                    break
-
-            # get values from rows
-            f.readline() # skip line right after the line with column names
-            line = f.readline() # first row
-            while(self._isrow(line)):
-                row_strs.append(line)
-                line = f.readline()
-
-            # get extra stuff
-            if extra:
-                extra_strs.append(line.strip())
-                extra_strs.extend([line.strip() for line in f.readlines() if line.strip()])
-
-        return {
-            'column' : column_str,
-            'row' : row_strs,
-            'extra' : extra_strs,
-        }
-
-    def _isrow(self, line: str) -> bool:
-        """ Returns True if the line passed is a valid row of output """
-        regex = '.*\|'
-        return bool(re.search(regex, line))
-
-    def _untabulate(self, row_dict: Dict) -> ColumnDict:
-        """
-        Returns a dictionary with columns as keys and row data as values
-        by formatting row strings.
-
-        :param row_dict: a dictionary with row strings
-        """
-
-        columns = [
-            column[1:].strip().lower() for column in row_dict['column'].split('|')
-            if len(column.strip()) > 0 # no null names as columns
-            ]
-        columnDict = {key: [] for key in columns} # type: ignore
-
-        for row in row_dict['row']:
-            rowdata = [data.strip().lower() for data in row.split('|') if len(data) > 0]
-            for data, column in zip(rowdata, columns):
-                if data:
-                    columnDict[column].append(data)
-                else:
-                    columnDict[column].append(None)
-
-        return columnDict
+        return query_utils._untabulate(query_utils._readrows(*args, **kwargs))
 
 class PostgresConnection(Connection):
     """ Class that handles interaction between user and PostgreSQL database server """
@@ -125,8 +53,13 @@ class PostgresConnection(Connection):
     dbtype = 'postgres'
 
     def _untabulate(self, row_dict: Dict) -> ColumnDict:
+        """
+        Makes a postgresql specific correction to the dictionary returned from
+        query_utils._untabulate. This function is to be used in Connection._readStdout
+        instead of query_utils._untabulate
+        """
 
-        columnDict = super()._untabulate(row_dict)
+        columnDict = query_utils._untabulate(row_dict)
 
         try:
             columnDict['extra'] = row_dict['extra']
@@ -134,6 +67,14 @@ class PostgresConnection(Connection):
             pass
 
         return columnDict
+
+    def _readStdout(self, *args, **kwargs) -> ColumnDict:
+        """
+        Uses Connection._untabulate instead of query_utils._untabulate.
+        Read Connection._untabulate documentation for more info.
+        """
+
+        return self._untabulate(query_utils._readrows(*args, **kwargs))
 
 class MysqlConnection(Connection):
     """ Class that handles interaction between user and MySQL database server """
--- dbmanage/query_utils.py
+++ dbmanage/query_utils.py
@@ -1,10 +1,90 @@
 
-""" A module for query parsing utilities """
+""" A module for query parsing and output reading utilities """
 
-from typing import Union
+import os
+import re
+
+from typing import Union, Dict, List
+
+ColumnDict = Dict[str, List[str]]
 
 # API
 
+#  Helper functions for reading output
+def _readrows(filepath: str, extra: bool = True) -> Dict:
+    """
+    Returns a dictionary with "column", "row", "extra" as keys
+    and the lines of the file that contain each one as dictionary values.
+
+    This function is to be called in self._readStdout and this code was
+    refactored so that _readStdout could be overwritten without the need of
+    rewriting the whole function.
+    """
+
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f'File with query output not found.\npath: {filepath}')
+
+    with open(filepath, 'r', encoding='utf-8') as f:
+
+        column_str = ''
+        row_strs = []
+        extra_strs = []
+
+        # read column line
+        column_str = f.readline()
+        while not _isrow(column_str):
+            column_str = f.readline()
+            if not column_str:
+                break
+
+        # get values from rows
+        f.readline() # skip line right after the line with column names
+        line = f.readline() # first row
+        while(_isrow(line)):
+            row_strs.append(line)
+            line = f.readline()
+
+        # get extra stuff
+        if extra:
+            extra_strs.append(line.strip())
+            extra_strs.extend([line.strip() for line in f.readlines() if line.strip()])
+
+    return {
+        'column' : column_str,
+        'row' : row_strs,
+        'extra' : extra_strs,
+    }
+
+def _isrow(line: str) -> bool:
+    """ Returns True if the line passed is a valid row of output """
+    regex = '.*\|'
+    return bool(re.search(regex, line))
+
+def _untabulate(row_dict: Dict) -> ColumnDict:
+    """
+    Returns a dictionary with columns as keys and row data as values
+    by formatting row strings.
+
+    :param row_dict: a dictionary with row strings
+    """
+
+    columns = [
+        column[1:].strip().lower() for column in row_dict['column'].split('|')
+        if len(column.strip()) > 0 # no null names as columns
+        ]
+    columnDict = {key: [] for key in columns} # type: ignore
+
+    for row in row_dict['row']:
+        rowdata = [data.strip().lower() for data in row.split('|') if len(data) > 0]
+        for data, column in zip(rowdata, columns):
+            if data:
+                columnDict[column].append(data)
+            else:
+                columnDict[column].append(None)
+
+    return columnDict
+
+#  Helper functions for connection requests
 def parse_connection_request(dbtype: str, **kwargs) -> str:
     """ Returns a string command to be run by a subprocess """
 
@@ -16,8 +96,6 @@ def parse_connection_request(dbtype: str, **kwargs) -> str:
         connection = _parse_psql_connection_request(**kwargs)
 
     return connection
-
-# helper functions
 
 def _parse_mysql_connection_request(host: str, user: str, dbname: str = '', passwd: str = '', **kwargs) -> str:
 
